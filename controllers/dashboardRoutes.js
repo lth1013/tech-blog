a3e7bda5c973bd70f66091be50e25e95ecc7324d
unknown <lukeharriman1013@gmail.com>
2023-08-11 10:51:08 -0700

    dashboard routes, server

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,2 +1,82 @@
 const router = require("express").Router();
-const { User, Post, Comment } = require("../../models");
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const router = require("./api/postRoutes");
+
+// GET all posts of the logged in user for dashboard
+
+router.get("/", (req, res) => {
+  console.log(req.session);
+
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username", "email"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username", "email"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      console.log(posts);
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username", "email"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username", "email"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render("edit-post", { post, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/new", (req, res) => {
+    res.render("new-post");
+    }
+);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv');
+
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const handlebars = require('express-handlebars');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection'); 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+require('models/Comment.js');
+require('models/Post.js');
+require('models/User.js');
+
+const app = express(); 
+const PORT = process.env.PORT || 3001; 
+
+const hb = handlebars.create({}); 
+
+const sessions = {
+  secret: 'Super secret secret',
+  cookie: {
+      maxAge: 3600000
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+      db: sequelize
+  })
+};
+
+app.use(session(sessions));
+
+app.engine('handlebars', hb.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('The app is now listening on PORT 3001'));
+});
